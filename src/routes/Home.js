e9422c274ca30b27d054fd606f0f102e5e95c0d7
brteo <matteo@meblabs.com>
2022-08-17 17:07:40 +0200

    chore: bootstrap

---
+++ src/routes/Home.js
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import React from 'react';
+
+const Home = () => (
+	<div className="inner-content">
+		<div className="d-grid gap-3">
+			<h1>Home</h1>
+		</div>
+	</div>
+);
+
+export default Home;
